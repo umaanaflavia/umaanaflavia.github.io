6b21de101471c0f8cf3631c30b3659b012df74b2
umaanaflavia <anaflavia20011@gmail.com>
2023-06-02 20:41:05 -0300

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+
+// PRELOADER
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+// MAIN NAVIGATION
+ $('.main-navigation').onePageNav({
+        scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
+        scrollOffset: 75, //Height of Navigation Bar
+        filter: ':not(.external)',
+        changeHash: true
+    }); 
+
+    /* NAVIGATION VISIBLE ON SCROLL */
+    mainNav();
+    $(window).scroll(function () {
+        mainNav();
+    });
+
+    function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        if (top > 40) $('.sticky-navigation').stop().animate({
+            "opacity": '1',
+            "top": '0'
+        });
+        else $('.sticky-navigation').stop().animate({
+            "opacity": '0',
+            "top": '-75'
+        });
+    }
+
+// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+   $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+// WOW ANIMATED 
+$(function()
+{
+    new WOW().init();
+})
---
+++ js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "30 april 2019 14:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});
